aacba48dc9e18453f680606c4a94d4c6c5555528
Dzarbis <dzarbis@gmail.com>
2021-04-03 20:05:59 -0400

    API is working and adds li with title and rating

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+var main = function() {
+    var searchTerm = document.querySelector("#title-search");
+    var search = searchTerm.value.trim();
+    var omdbApi = "http://www.omdbapi.com/?apikey=ace027ad&t=" + search;
+
+    console.log(search);
+    fetch(omdbApi).then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    
+
+    var ratingLocation = document.querySelector("#landing-spot");
+
+    var rating = document.createElement("li");
+    var ratingPull = response.imdbRating;
+    rating.textContent = search + " - Rating: " + ratingPull;    
+    ratingLocation.appendChild(rating);
+    })
+};
